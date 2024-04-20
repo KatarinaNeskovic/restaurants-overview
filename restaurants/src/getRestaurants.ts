de2bba4f05c0a711ec4392f64b2cc615c49bacf1
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-20 16:54:05 +0200

    write get restaurants function

--- restaurants/src/getRestaurants.ts
+++ restaurants/src/getRestaurants.ts
@@ -1,8 +1,21 @@
 import type { IData, IRestaurant } from "./types";
 
 export function getRestaurants(data: IData): IRestaurant[] {
-  const result = data.restaurants.map ()=> {
-    
-  };
-  return [];
+  const result = data.restaurants.map((el) => {
+    const restaurant: IRestaurant = {
+      name: el.name,
+      address: {
+        firstLine: el.address.firstLine,
+        city: el.address.city,
+        postalCode: el.address.postalCode,
+      },
+      cuisines: el.cuisines.map((e) => 
+        ({name:e.name})),
+      rating: {
+        starRating: el.rating.starRating,
+      },
+    };
+    return restaurant;
+  });
+  return result;
 }
