5b3eb4663279e34bf3b668bdf22171ba62b568d4
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-20 15:47:44 +0200

    create additional types

--- restaurants/src/getRestaurants.ts
+++ restaurants/src/getRestaurants.ts
@@ -1,6 +1,8 @@
-export function getRestaurants(data:Data):Restaurant[] {
-  
-    return (
+import type { IData, IRestaurant } from "./types";
 
-    )
+export function getRestaurants(data: IData): IRestaurant[] {
+  const result = data.restaurants.map ()=> {
+    
+  };
+  return [];
 }
