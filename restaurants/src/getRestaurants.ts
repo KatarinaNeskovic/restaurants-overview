7beaca1fc6761759c43cd56330370120ee7e7868
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-20 15:47:04 +0200

    add types

---
+++ restaurants/src/getRestaurants.ts
@@ -0,0 +1,6 @@
+export function getRestaurants(data:Data):Restaurant[] {
+  
+    return (
+
+    )
+}
--- restaurants/src/types.ts
+++ restaurants/src/types.ts
@@ -1,16 +1,35 @@
-interface Data {
-    metaData: unknown;
-    restaurants: Restaurant[];
-    deliveryFees: unknown;
-    promotedPlacement: unknown;
-    filters: unknown;
-    layout: unknown;
-    enrichedLists: unknown;
+export interface IData {
+  metaData: unknown;
+  restaurants: IRestaurant[];
+  deliveryFees: unknown;
+  promotedPlacement: unknown;
+  filters: unknown;
+  layout: unknown;
+  enrichedLists: unknown;
 }
 
-interface Restaurant {
+export interface IRestaurant
+ {
   name: string;
-  cuisines: string;
-  rating: number;
-  address: string;
+  cuisines: ICuisine[];
+  rating: IRating;
+  address: IAddress;
+}
+
+export interface IAddress {
+  city: string;
+  firstLine: string;
+  postalCode: string;
+  location: unknown; //setting as unknown to avoid extra type definition for what I won't need.
+}
+
+export interface IRating {
+    count:number;
+    starRating: number;
+    userRating: number;
+}
+
+export interface ICuisine {
+    name:string;
+    uniqueName:string;
 }
