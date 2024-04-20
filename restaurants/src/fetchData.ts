c7da0f779f6efceb2ad85fbcdacb96a59aa2b04d
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-20 18:03:23 +0200

    write fetch function

--- restaurants/src/fetchData.ts
+++ restaurants/src/fetchData.ts
@@ -1,2 +1,21 @@
+import { getRestaurants } from "./getRestaurants";
+import { IRestaurant } from "./types";
+
 const apiUrl: string =
   "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/CF118AZ";
+
+export async function fetchData(): Promise<IRestaurant[]> {
+  const response = await fetch(apiUrl)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Error fetching data");
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      throw new Error("An error occured while fetching data" + error.message);
+    });
+  const restaurantData: IRestaurant[] = getRestaurants(response);
+
+  return restaurantData.slice(0, 11);
+}
