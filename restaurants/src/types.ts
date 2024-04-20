e15a2b9f9d1379ab260e6deaa0138e5752c2bc7e
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-20 16:53:50 +0200

    edit types

--- restaurants/src/types.ts
+++ restaurants/src/types.ts
@@ -20,16 +20,16 @@ export interface IAddress {
   city: string;
   firstLine: string;
   postalCode: string;
-  location: unknown; //setting as unknown to avoid extra type definition for what I won't need.
+  location?: unknown; //setting as unknown to avoid extra type definition for what I won't need.
 }
 
 export interface IRating {
-    count:number;
+    count?:number;
     starRating: number;
-    userRating: number;
+    userRating?: number;
 }
 
 export interface ICuisine {
     name:string;
-    uniqueName:string;
+    uniqueName?:string;
 }
