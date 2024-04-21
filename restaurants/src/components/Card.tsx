afc61ad5988c813320bb2671cbab408c6dacbf99
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-21 20:55:54 +0200

    create card to hold restaurant data

---
+++ restaurants/src/components/Card.tsx
@@ -0,0 +1,36 @@
+import { IRestaurant } from "../types/types";
+
+interface Props {
+  restaurant: IRestaurant;
+}
+export const Card: React.FC<Props> = ({ restaurant }) => {
+  const { name, rating, cuisines, address } = restaurant;
+  const { city, firstLine, postalCode, location } = address;
+  const { coordinates, type } = location;
+
+  return (
+    <div className="card">
+      <div className="name"> Name: {name} </div>
+      <div className="rating"> Rating: {rating.starRating} </div>
+      <div className="cuisines">
+        {" "}
+        Cuisines: {cuisines.map((c) => c.name).join(", ")}{" "}
+      </div>
+      <div className="address">
+        {" "}
+        Address:
+        <ul>
+          <li> Street: {firstLine} </li>
+          <li> City: {city} </li>
+          <li> Postal Code: {postalCode} </li>
+          <li>
+            {" "}
+            Location:
+            <ul> {type}</ul>
+            <ul> {[coordinates].join(", ")}</ul>
+          </li>
+        </ul>{" "}
+      </div>
+    </div>
+  );
+};
