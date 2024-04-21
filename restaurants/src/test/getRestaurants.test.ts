05f88fd6c868d10af90f25cb7e65c4cdfb10b5a3
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-21 19:14:48 +0200

    formatting

--- restaurants/src/test/getRestaurants.test.ts
+++ restaurants/src/test/getRestaurants.test.ts
@@ -17,68 +17,68 @@ describe("Test getRestaurants function ", () => {
           rating: { starRating: 3.8 },
         },
         {
-            address: {
-              city: "London",
-              firstLine: "Lane 3",
-              postalCode: "2245",
-            },
-            cuisines: [{ name: "vegan" }, { name: "burger" }, { name: "salad" }],
-            name: "Johnnys",
-            rating: { starRating: 4.2 },
+          address: {
+            city: "London",
+            firstLine: "Lane 3",
+            postalCode: "2245",
           },
-          {
-            address: {
-              city: "Brighton",
-              firstLine: "Bedford 345",
-              postalCode: "2345",
-            },
-            cuisines: [{ name: "japanese" }, { name: "sushi" }, { name: "fusion" }],
-            name: "Johnnys",
-            rating: { starRating: 4.8 },
+          cuisines: [{ name: "vegan" }, { name: "burger" }, { name: "salad" }],
+          name: "Johnnys",
+          rating: { starRating: 4.2 },
+        },
+        {
+          address: {
+            city: "Brighton",
+            firstLine: "Bedford 345",
+            postalCode: "2345",
           },
-
+          cuisines: [
+            { name: "japanese" },
+            { name: "sushi" },
+            { name: "fusion" },
+          ],
+          name: "Johnnys",
+          rating: { starRating: 4.8 },
+        },
       ],
-      deliveryFees:12345,
-      enrichedLists: 'n/a',
-      filters:'n/a',
-      layout:'n/a',
-      promotedPlacement:3
+      deliveryFees: 12345,
+      enrichedLists: "n/a",
+      filters: "n/a",
+      layout: "n/a",
+      promotedPlacement: 3,
     };
     const result = getRestaurants(data);
-    expect(result).toEqual(
-      [
-        {
-            address: {
-              city: "Belfast",
-              firstLine: "Norwich 25",
-              postalCode: "emc2",
-            },
-            cuisines: [{ name: "thai" }, { name: "pizza" }, { name: "burger" }],
-            name: "Johnnys",
-            rating: { starRating: 3.8 },
-          },
-          {
-              address: {
-                city: "London",
-                firstLine: "Lane 3",
-                postalCode: "2245",
-              },
-              cuisines: [{ name: "vegan" }, { name: "burger" }, { name: "salad" }],
-              name: "Johnnys",
-              rating: { starRating: 4.2 },
-            },
-            {
-              address: {
-                city: "Brighton",
-                firstLine: "Bedford 345",
-                postalCode: "2345",
-              },
-              cuisines: [{ name: "japanese" }, { name: "sushi" }, { name: "fusion" }],
-              name: "Johnnys",
-              rating: { starRating: 4.8 },
-            },  
-        ]
-    )
+    expect(result).toEqual([
+      {
+        address: {
+          city: "Belfast",
+          firstLine: "Norwich 25",
+          postalCode: "emc2",
+        },
+        cuisines: [{ name: "thai" }, { name: "pizza" }, { name: "burger" }],
+        name: "Johnnys",
+        rating: { starRating: 3.8 },
+      },
+      {
+        address: {
+          city: "London",
+          firstLine: "Lane 3",
+          postalCode: "2245",
+        },
+        cuisines: [{ name: "vegan" }, { name: "burger" }, { name: "salad" }],
+        name: "Johnnys",
+        rating: { starRating: 4.2 },
+      },
+      {
+        address: {
+          city: "Brighton",
+          firstLine: "Bedford 345",
+          postalCode: "2345",
+        },
+        cuisines: [{ name: "japanese" }, { name: "sushi" }, { name: "fusion" }],
+        name: "Johnnys",
+        rating: { starRating: 4.8 },
+      },
+    ]);
   });
-
 });
