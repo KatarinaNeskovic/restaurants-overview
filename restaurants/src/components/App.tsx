269ebbaf09c928f2eda752742616003e55688add
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-04-22 11:47:30 +0200

    write Restaurant component

--- restaurants/src/components/App.tsx
+++ restaurants/src/components/App.tsx
@@ -1,9 +1,31 @@
-import './App.css'
+import { useState } from "react";
+import { fetchData } from "../functions/fetchData";
+import { IRestaurant } from "../types/types";
+import "./App.css";
+import { Card } from "./Card";
 
-function App() {
+const Restaurants = () => {
+  const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
+
+  const handleClick = async () => {
+    try {
+      const restaurantsArray = await fetchData();
+      setRestaurants(restaurantsArray);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
-  <h1> Hello world </h1>
-  )
-}
+    <div>
+      <button onClick={handleClick}>Fetch Data</button>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant, index) => (
+          <Card key={index} restaurant={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default Restaurants;
