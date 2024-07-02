8d0045e69ef10ef4c81e84d0bac0775ac7952c14
katarina_neskovic <katarina.neskovic@dpa-info.com>
2024-07-02 16:49:42 +0200

    sort function

--- src/functions/fetchData.ts
+++ src/functions/fetchData.ts
@@ -5,7 +5,7 @@ const apiUrl: string =
   "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/CF118AZ";
 
 export async function fetchData(): Promise<IRestaurant[]> {
-    const response = await fetch(apiUrl)
+  const response = await fetch(apiUrl)
     .then((res) => {
       if (!res.ok) {
         throw new Error("Error fetching data");
@@ -18,7 +18,10 @@ export async function fetchData(): Promise<IRestaurant[]> {
 
   const restaurantData: IRestaurant[] = getRestaurants(response);
 
-  return restaurantData.slice(0, 10);
+  return restaurantData.slice(0, 10).sort((r1, r2) => {
+    if (r1.rating.starRating > r2.rating.starRating) return -1;
+    else if (r1.rating.starRating < r2.rating.starRating) return 1;
+    else return 0;
+  });
 }
-
-
+//typecats
--- src/types/types.ts
+++ src/types/types.ts
@@ -34,6 +34,6 @@ export interface ICuisine {
 }
 
 export interface ILocation {
-  type:string,
-  coordinates: [number,number]
+  type: string;
+  coordinates: [number, number];
 }
